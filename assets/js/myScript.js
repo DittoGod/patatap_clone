a73c5f327207ffbc1253ffa972938cc9011bf563
DittoGod <dittogod1@gmail.com>
2019-08-15 13:41:10 +0100

    practice

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -8,18 +8,13 @@
 //  // Note the plus operator on Point objects.
 //  // PaperScript does that for us, and much more!
 //  path.lineTo(start + [500, -50]);
-
-var myCircle = new Path.Circle(new Point(0, 0), 10);
-myCircle.fillColor = 'orange';
-
-var grid = [];
 var x = 0;
 var y = 0;
 
 for (i = 0; i < 10; i++) {
     for (j = 0; j < 10; j++) {
         new Path.Circle(new Point(x, y), 10).fillColor = 'orange';
-        console.log(x + " " + y);
+        // console.log(x + " " + y);
         y += 100;
     }
     y = 0;
